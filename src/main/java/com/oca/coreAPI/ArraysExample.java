13d8408c123e2571d0900b6d05de044d1f682126
mcrassin <mathieu.crassin@gmail.com>
2016-04-24 23:45:35 +0200

    core API examples.

--- src/main/java/com/oca/coreAPI/ArraysExample.java
+++ src/main/java/com/oca/coreAPI/ArraysExample.java
@@ -1,6 +1,7 @@
 package com.oca.coreAPI;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author mcrassin
@@ -9,7 +10,7 @@ import java.util.Arrays;
 public class ArraysExample {
 
     public static void main(String[] args) {
-        int[] tab = {1, 4, 6, 9, 16, 3, 45, 2, 99};
+        Integer[] tab = {1, 4, 6, 9, 16, 3, 45, 2, 99};
         displayTab(tab);
         System.out.println("binary search of 2 of non sorted tab: " + java.util.Arrays.binarySearch(tab, 2));
         System.out.println();
@@ -17,13 +18,29 @@ public class ArraysExample {
         Arrays.sort(tab);
         displayTab(tab);
         System.out.println("binary search of 2 of sorted tab: " + java.util.Arrays.binarySearch(tab, 2));
+        System.out.println();
 
+        // convert to list
+        System.out.println("backed list of our array");
+        List<Integer> list = Arrays.asList(tab);
+        displayTab(list);
+        System.out.println("replace 1 by 11 at index 0.");
+        tab[0] = 11;
+        displayTab(list);
+        System.out.println("value is replaced inside in array automatically");
     }
 
-    private static void displayTab(int[] tab) {
+    private static void displayTab(Integer[] tab) {
         System.out.print("{");
         for (int i=0; i<tab.length; i++) {
             System.out.print(tab[i] + ",");
+        }
+        System.out.println("}");
+    }
+    private static void displayTab(List list) {
+        System.out.print("{");
+        for (Object i : list) {
+            System.out.print(i + ",");
         }
         System.out.println("}");
     }
---
+++ src/main/java/com/oca/coreAPI/Dates.java
@@ -0,0 +1,15 @@
+package com.oca.coreAPI;
+
+/**
+ * @author mcrassin
+ * @since 24/04/2016
+ */
+public class Dates {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
